17c67ce4446333481451c14306f12c39e2a23663
javier162380 <javierllorente16@gmail.com>
2020-04-04 23:14:39 +0200

    adding pretty much all chagnes

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/Javier162380/go-db-transfer
 go 1.13
 
 require (
-	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v0.0.7
-	github.com/spf13/viper v1.4.0
+	gopkg.in/yaml.v2 v2.2.2
 )
--- main.go
+++ main.go
@@ -1,22 +1,15 @@
-/*
-Copyright © 2020 Javier Llorente
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package main
 
-import "github.com/Javier162380/go-db-transfer/cmd"
+import (
+	"fmt"
+
+	"github.com/Javier162380/go-db-transfer/configuration"
+	"github.com/Javier162380/go-db-trasfer/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	cmd.Init()
+	load := configuration.LoadConfiguration(cmd.CONFIGFILE)
+	fmt.Printf("%s", load)
+
 }
