838e100c8542470ae02096e11e38bec016ebc111
javier162380 <javierllorente16@gmail.com>
2020-04-06 00:48:53 +0200

    solve bug with sql string interpolation

--- db/client.go
+++ db/client.go
@@ -6,26 +6,29 @@ import (
 	"log"
 	"os"
 
+	//postgresql driver
 	_ "github.com/lib/pq"
 )
 
-type DBClient struct {
-	Uri             string
+// Client main db fields
+type Client struct {
+	URI             string
 	Dbtype          string
 	Targetschema    string
 	Connection      *sql.DB
 	Tmptargetschema string
 }
 
-func (dbc *DBClient) Connect() {
+// Connect to sql db
+func (dbc *Client) Connect() {
 
 	switch {
 	case dbc.Dbtype == "postgres":
 		var err error
-		dbc.Connection, err = sql.Open("postgres", dbc.Uri)
+		dbc.Connection, err = sql.Open("postgres", dbc.URI)
 
 		if err != nil {
-			log.Fatal("Unable to connect to DB %s", err)
+			log.Fatal("Unable to connect to DB ", err)
 			os.Exit(1)
 		}
 
@@ -37,21 +40,27 @@ func (dbc *DBClient) Connect() {
 	}
 }
 
-func ReplicateSchema(input_db DBClient, output_db DBClient) {
+// ReplicateSchema a schema between to databases
+func ReplicateSchema(inputdb Client, outputdb Client) {
 
-	tx, err := output_db.Connection.Begin()
+	tx, err := outputdb.Connection.Begin()
 
 	if err != nil {
-		log.Fatal("Unabled to connect to outputdb", err)
-		os.Exit(1)
+		log.Fatal("Unabled to connect to outputdb ", err)
 	}
-	tx.Exec("CREATE SCHEMA $1", output_db.Tmptargetschema)
 
-	rows, err := input_db.Connection.Query("SELECT table_name FROM information_schema.tables WHERE table_schema=$1", input_db.Targetschema)
+	tableschemacreation := fmt.Sprintf("CREATE SCHEMA %s", outputdb.Tmptargetschema)
+
+	if _, err := tx.Exec(tableschemacreation); err != nil {
+		log.Fatal("Unable to create db schema ", err)
+	}
+
+	rows, err := inputdb.Connection.Query(`SELECT table_name 
+										   FROM information_schema.tables 
+										   WHERE table_schema=$1`, inputdb.Targetschema)
 
 	if err != nil {
-		log.Fatal("Unable to retrieve schema data %s", rows)
-		os.Exit(1)
+		log.Fatal("Unable to retrieve schema data ", err)
 	}
 
 	defer rows.Close()
@@ -61,21 +70,27 @@ func ReplicateSchema(input_db DBClient, output_db DBClient) {
 			log.Fatal(err)
 		}
 
-		tablemetadata := gettablemetadata(input_db, input_db.Targetschema, tablename)
-		statement := createtablestatement(tablemetadata, tablename, output_db.Tmptargetschema)
-		tx.Exec(statement)
+		tablemetadata := gettablemetadata(inputdb, inputdb.Targetschema, tablename)
+		statement := createtablestatement(tablemetadata, tablename, outputdb.Tmptargetschema)
+		if _, err := tx.Exec(statement); err != nil {
+			log.Printf("Unable to create table %s  %s", tablename, err)
+			tx.Rollback()
+		}
 
 	}
 
 	tx.Commit()
 }
 
-func gettablemetadata(db DBClient, tableschema string, tablename string) map[string]string {
+func gettablemetadata(db Client, tableschema string, tablename string) map[string]string {
 
-	rows, err := db.Connection.Query("SELECT column_name, data_type FROM information_schema.columns WHERE table_name =$1 AND table_schema=$2", tablename, tableschema)
+	rows, err := db.Connection.Query(`SELECT column_name, data_type 
+	                                  FROM information_schema.columns 
+									  WHERE table_name =$1 AND table_schema=$2`,
+		tablename, tableschema)
 
 	if err != nil {
-		log.Fatal("Unable to retrieve schema data %s", rows)
+		log.Fatal("Unable to retrieve schema data ", rows)
 		os.Exit(1)
 	}
 
@@ -105,10 +120,10 @@ func createtablestatement(tablemetadata map[string]string, tablename string, tab
 
 	var fieldstring string
 	for column, datetype := range tablemetadata {
-		fieldstring += fmt.Sprintf("\"%s\" %s, ", column, datetype)
+		fieldstring += fmt.Sprintf("\"%s\" %s ,", column, datetype)
 
 	}
 
-	return fmt.Sprintf("CREATE TABLE %s.%s (%s)", tableschema, tablename, fieldstring)
+	return fmt.Sprintf("CREATE TABLE %s.%s (%s)", tableschema, tablename, fieldstring[:len(fieldstring)-1])
 
 }
