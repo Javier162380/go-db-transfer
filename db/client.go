2f79d148b055fd0bfa97841af53bbf0a49a1e0e2
javier162380 <javierllorente16@gmail.com>
2020-04-05 13:10:27 +0200

    solve bug with dbtype switch

--- db/client.go
+++ db/client.go
@@ -2,8 +2,8 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -18,13 +18,14 @@ type DBClient struct {
 func (dbc *DBClient) Connect() {
 
 	switch {
-	case dbc.Dbtype == "postrges":
-		conn, err := sql.Open("postgres", dbc.Uri)
+	case dbc.Dbtype == "postgres":
+		var err error
+		dbc.Connection, err = sql.Open("postgres", dbc.Uri)
 
 		if err != nil {
 			log.Fatal("Unable to connect to DB %s", err)
+			os.Exit(1)
 		}
-		dbc.Connection = conn
 
 	case dbc.Dbtype != "postgres":
 		log.Fatal("DB type not supportted currently")
@@ -37,8 +38,17 @@ func ReplicateSchema(input_db DBClient, output_db DBClient) {
 	rows, err := input_db.Connection.Query("SELECT table_name FROM information_schema.tables WHERE table_schema=$1", input_db.Targetschema)
 
 	if err != nil {
-
+		log.Fatal("Unable to retrieve schema data %s", rows)
+		os.Exit(1)
 	}
 
-	fmt.Printf("%s", rows)
+	defer rows.Close()
+	for rows.Next() {
+		var table_name string
+		if err := rows.Scan(&table_name); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("table name %s\n", table_name)
+
+	}
 }
